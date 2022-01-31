5281e22e1cd8da2711d2a9d95fc6662ff88b21d7
Patrick Quilty <pquilty79@gmail.com>
2022-01-31 00:42:08 -0600

    most coffee functionality done

--- README.md
+++ README.md
@@ -17,32 +17,33 @@ improvements to it.
 
 - Update the HTML
 
-    Tables are a little old school, you need to refactor the code so that each
+    ~~Tables are a little old school, you need to refactor the code so that each
     coffee is displayed in a `div` that contains a heading displaying the coffee
     name, and the type of roast in a paragraph. Don't display the ids, these are
-    only for our application's internal use
+    only for our application's internal use~~
 
-- When the page loads, the coffees should be sorted by their ids in ascending
-  order
+~~- When the page loads, the coffees should be sorted by their ids in ascending
+  order~~
 
-- Add functionality to search through the coffees by name, and display only the
+~~- Add functionality to search through the coffees by name, and display only the
   coffees that match the provided search term (You will need to add an input
-  field to the existing form for this)
+  field to the existing form for this)~~
 
-- Add functionality to update the displayed coffee as the user types into the
-  search box, or as soon as they select an option from the select.
+~~- Add functionality to update the displayed coffee as the user types into the
+  search box, or as soon as they select an option from the select.~~
 
 **Bonus**
 
-- Add an option to select all roasts for roast type
+~~- Add an option to select all roasts for roast type~~
 
-- Make your name search case insensitive
+~~- Make your name search case insensitive~~
 
-- Allow the user to add new coffees to the page
+~~- Allow the user to add new coffees to the page
 
     Create another form on the page that has an input for the coffee name, and
     a select to choose the coffee roast. When the form is submitted, the new
-    coffee should appear on the page. (*Note that any new coffees you add will
+    coffee should appear on the page.~~ 
+- (*Note that any new coffees you add will
     be lost when you refresh the page, for an extra challenge, research
     how `localStorage` works and see if you can find a way to persist the data*)
 
--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 "use strict"
 
-function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
 
+function renderCoffee(coffee) {
+    let html = '<div class="coffee_display d-block mt-4">';
+    html += '<div class="d-flex align-items-baseline">';
+    html += '<h2 class="name mr-3">' + coffee.name + '</h2>';
+    html += '<p class="roast mr-5">' + coffee.roast + '</p>';
+    html += '</div>'
+    html += '</div>'
     return html;
 }
 
 function renderCoffees(coffees) {
-    var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
+    let html = '';
+    for(let i = 0; i <= coffees.length - 1; i++) {
         html += renderCoffee(coffees[i]);
     }
     return html;
@@ -20,18 +21,39 @@ function renderCoffees(coffees) {
 
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
-    var filteredCoffees = [];
+    let selectedRoast = roastSelection.value;
+    let namedCoffee = coffeeName.value.toLowerCase();
+    let filteredCoffees = [];
     coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
+        if (namedCoffee === "") {
+         if (coffee.roast === selectedRoast) {
+                filteredCoffees.push(coffee);
+            } else if (selectedRoast === 'all') {
+                filteredCoffees.push(coffee);
+            }
+        }
+         else if (coffee.name.toLowerCase().includes(namedCoffee)) {
             filteredCoffees.push(coffee);
         }
     });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
+    coffee_display.innerHTML = renderCoffees(filteredCoffees);
 }
 
+function newCoffee (id, name, roast) {
+    name = newCoffeeName.value;
+    roast = newCoffeeRoast.value;
+    id = coffees.length + 1;
+    let coffee = {id, name, roast};
+    coffees.push(coffee);
+    // localStorage.setItem(name, roast); Not working
+    console.log(coffee) //test to see if id is correct//
+    coffee_display.innerHTML = renderCoffees(coffees);
+}
+
+
+
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-var coffees = [
+let coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
     {id: 2, name: 'Half City', roast: 'light'},
     {id: 3, name: 'Cinnamon', roast: 'light'},
@@ -48,10 +70,17 @@ var coffees = [
     {id: 14, name: 'French', roast: 'dark'},
 ];
 
-var tbody = document.querySelector('#coffees');
-var submitButton = document.querySelector('#submit');
-var roastSelection = document.querySelector('#roast-selection');
+let coffee_display = document.querySelector('#coffees');
+let submitButton = document.querySelector('#submit');
+let roastSelection = document.querySelector('#roast-selection');
+let coffeeName = document.querySelector('#coffee-name');
+let newCoffeeName = document.querySelector('#newCoffeeName');
+let newCoffeeRoast = document.querySelector('#newCoffeeRoast');
+let submitToo = document.querySelector('#submitToo')
 
-tbody.innerHTML = renderCoffees(coffees);
+
+coffee_display.innerHTML = renderCoffees(coffees);
 
 submitButton.addEventListener('click', updateCoffees);
+submitToo.addEventListener('click', newCoffee);
+coffeeName.addEventListener('keyup', updateCoffees)
